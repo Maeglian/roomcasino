92fab143a75a62b2ad5b8628806619f8cd77f16f
mariaisaeva <misaeva@wakeapp.ru>
2020-10-15 21:26:11 +0300

    added ProfilePage

---
+++ src/pages/ProfilePage.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="CabinetPage ProfilePage">
+    <div class="CabinetPage-Title ProfilePage-Title">
+      {{ title }}
+    </div>
+    <div class="CabinetPage-Tabs">
+      <router-link to="/cabinet/profile/general" class="CabinetPage-Tab">
+        General
+      </router-link>
+      <router-link to="/cabinet/profile/security" class="CabinetPage-Tab">
+        Security
+      </router-link>
+    </div>
+    <router-view :key="$route.path" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProfilePage',
+  props: {
+    title: {
+      type: String,
+      required: false,
+      default: '',
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.ProfilePage {
+  &-Content {
+    margin-top: 18px;
+    width: 100%;
+
+    @media (min-width: $screen-m) {
+      margin-top: 24px;
+    }
+  }
+}
+
+</style>
