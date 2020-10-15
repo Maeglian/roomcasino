40ba867ae38cd5429af7238771b477692254aa9f
mariaisaeva <misaeva@wakeapp.ru>
2020-10-15 12:48:53 +0300

    added arrows for pagination

--- src/App.vue
+++ src/App.vue
@@ -34,10 +34,10 @@ export default {
   --color-hover: #C40916;
   --color-accept: #67B12D;
   --color-discard: #CC1F1F;
+  --color-border-ghost: rgba(110, 115, 130, 0.5);
   --color-faded: rgba(255, 255, 255, 0.5);
   --color-more-faded: rgba(255, 255, 255, 0.4);
   --color-most-faded: rgba(255, 255, 255, 0.2);
-  --color-border-ghost: rgba(58, 63, 85, 0.3);
   --color-bg: #0E152F;
   --color-bg-lighter: #1B2138;
   --color-bg-darker: rgba(39, 43, 95, 0.5);
