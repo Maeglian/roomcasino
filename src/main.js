c35f231b09560432b15c59f3ca4c50dc8bb797be
mariaisaeva <misaeva@wakeapp.ru>
2020-10-19 23:24:37 +0300

    added vue-modal and vuelidate

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import '@/styles/main.scss';
 import Vue from 'vue';
 import { Plugin } from 'vue-fragment';
+import VModal from 'vue-js-modal';
 import vClickOutside from 'v-click-outside';
+import Vuelidate from 'vuelidate';
 import App from './App.vue';
 import store from './store';
 import router from './router';
@@ -10,6 +12,8 @@ Vue.prototype.$skin = 'VegasWinner';
 
 Vue.use(Plugin);
 Vue.use(vClickOutside);
+Vue.use(Vuelidate);
+Vue.use(VModal);
 
 new Vue({
   router,
