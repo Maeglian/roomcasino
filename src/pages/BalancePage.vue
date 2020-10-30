4e30e6680037696863ce6a2789cf4a77ec82125f
mariaisaeva <misaeva@wakeapp.ru>
2020-10-30 20:08:33 +0300

    added currency in BalancePage

--- src/pages/BalancePage.vue
+++ src/pages/BalancePage.vue
@@ -23,110 +23,35 @@
         <div class="Table-Cell BalancePage-Cell CabinetPage-Cell CabinetPage-Th">
         </div>
       </div>
-      <div class="Table-Row CabinetPage-Row">
+      <div
+        v-for="(acc, i) in userAccounts"
+        :key="i"
+        class="Table-Row CabinetPage-Row"
+      >
         <div class="Table-Cell BalancePage-Cell CabinetPage-Cell BalancePage-Active">
           <label class="CabinetPage-Label">
-            Active
+            {{ acc.active ? 'Active' : 'Select' }}
             <input
               class="CabinetPage-Checkbox"
               type="radio"
-              name="currency"
-              value=""
+              name="account"
+              checked="acc.active"
+              :value="acc.currency"
             >
             <span class="CabinetPage-Checkmark CabinetPage-Checkmark--radio"></span>
           </label>
         </div>
         <div class="Table-Cell BalancePage-Cell CabinetPage-Cell BalancePage-Currency">
-          Usd
+          {{ acc.currency }}
         </div>
         <div class="Table-Cell BalancePage-Cell CabinetPage-Cell BalancePage-Amount">
-          514.00 Usd
+          {{ acc.balance }} {{ acc.currency }}
         </div>
         <div class="Table-Cell BalancePage-Cell CabinetPage-Cell BalancePage-Locked">
           0
         </div>
         <div class="Table-Cell BalancePage-Cell CabinetPage-Cell BalancePage-Cash">
-          316.00 Usd
-        </div>
-        <div class="Table-Cell BalancePage-Cell CabinetPage-Cell BalancePage-Btns">
-          <button
-            class="Btn Btn--color CabinetPage-Btn BalancePage-DepositBtn"
-            @click="$modal.show('cashier')"
-          >
-            Deposit
-          </button>
-          <button
-            class="Btn Btn--outline CabinetPage-Btn"
-            @click="onClickCashout()"
-          >
-            Cashout
-          </button>
-        </div>
-      </div>
-      <div class="Table-Row CabinetPage-Row">
-        <div class="Table-Cell BalancePage-Cell CabinetPage-Cell BalancePage-Active">
-          <label class="CabinetPage-Label">
-            Select
-            <input
-              class="CabinetPage-Checkbox"
-              type="radio"
-              name="currency"
-              value=""
-            >
-            <span class="CabinetPage-Checkmark CabinetPage-Checkmark--radio"></span>
-          </label>
-        </div>
-        <div class="Table-Cell BalancePage-Cell CabinetPage-Cell BalancePage-Currency">
-          Usd
-        </div>
-        <div class="Table-Cell BalancePage-Cell CabinetPage-Cell BalancePage-Amount">
-          514.00 Usd
-        </div>
-        <div class="Table-Cell BalancePage-Cell CabinetPage-Cell BalancePage-Locked">
           0
-        </div>
-        <div class="Table-Cell BalancePage-Cell CabinetPage-Cell BalancePage-Cash">
-          316.00 Usd
-        </div>
-        <div class="Table-Cell BalancePage-Cell CabinetPage-Cell BalancePage-Btns">
-          <button
-            class="Btn Btn--color CabinetPage-Btn BalancePage-DepositBtn"
-            @click="$modal.show('cashier')"
-          >
-            Deposit
-          </button>
-          <button
-            class="Btn Btn--outline CabinetPage-Btn"
-            @click="onClickCashout()"
-          >
-            Cashout
-          </button>
-        </div>
-      </div>
-      <div class="Table-Row CabinetPage-Row">
-        <div class="Table-Cell BalancePage-Cell CabinetPage-Cell BalancePage-Active">
-          <label class="CabinetPage-Label">
-            Select
-            <input
-              class="CabinetPage-Checkbox"
-              type="radio"
-              name="currency"
-              value=""
-            >
-            <span class="CabinetPage-Checkmark CabinetPage-Checkmark--radio"></span>
-          </label>
-        </div>
-        <div class="Table-Cell BalancePage-Cell CabinetPage-Cell BalancePage-Currency">
-          Usd
-        </div>
-        <div class="Table-Cell BalancePage-Cell CabinetPage-Cell BalancePage-Amount">
-          514.00 Usd
-        </div>
-        <div class="Table-Cell BalancePage-Cell CabinetPage-Cell BalancePage-Locked">
-          0
-        </div>
-        <div class="Table-Cell BalancePage-Cell CabinetPage-Cell BalancePage-Cash">
-          316.00 Usd
         </div>
         <div class="Table-Cell BalancePage-Cell CabinetPage-Cell BalancePage-Btns">
           <button
@@ -144,7 +69,10 @@
         </div>
       </div>
     </div>
-    <button class="CabinetPage-AddBtn">
+    <button
+      class="CabinetPage-AddBtn"
+      @click="$modal.show('addCurrency')"
+    >
       <span class="CabinetPage-AddBtnPlus CabinetPage-AddBtnPlus--left">
         +
       </span>
@@ -152,11 +80,27 @@
         Add currency
       </span>
     </button>
+    <modal name="addCurrency" width="348" height="auto">
+      <div class="AddCurrency">
+        <div class="CabinetPage-Header AddCurrency-Header">
+          Add Currency
+        </div>
+        <div class="AddCurrency-Content">
+          <div
+            v-for="cur in currencyList"
+            :key="cur"
+            class="AddCurrency-Currency"
+          >
+            {{ cur }}
+          </div>
+        </div>
+      </div>
+    </modal>
   </div>
 </template>
 
 <script>
-import { mapMutations } from 'vuex';
+import { mapMutations, mapState } from 'vuex';
 
 export default {
   name: 'BalancePage',
@@ -165,6 +109,33 @@ export default {
       type: String,
       required: false,
       default: '',
+    },
+  },
+  data() {
+    return {
+      fakeUserAccounts: [
+        {
+          active: true,
+          currency: 'USD',
+          balance: '514.00',
+        },
+        {
+          active: false,
+          currency: 'EUR',
+          balance: '318.00',
+        },
+        {
+          active: false,
+          currency: 'RUB',
+          balance: '0',
+        },
+      ],
+    };
+  },
+  computed: {
+    ...mapState(['user', 'currencyList']),
+    userAccounts() {
+      return this.user.accountList || this.fakeUserAccounts;
     },
   },
   methods: {
@@ -250,6 +221,34 @@ export default {
 
   &-Cash:before {
     content: 'Available to cash out';
+  }
+}
+
+.AddCurrency {
+  background: var(--color-body);
+
+  &-Content {
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    grid-gap: 4px;
+    padding: 16px;
+    background: var(--color-bg);
+  }
+
+  &-Currency {
+    padding: 13px 0;
+    font-size: 10px;
+    font-weight: 700;
+    text-transform: uppercase;
+    text-align: center;
+    color: var(--color-text-main);
+    background: var(--color-bg-lighter);
+    cursor: pointer;
+
+    &:hover {
+      padding: 11px 0;
+      border: 2px solid var(--color-main1);
+    }
   }
 }
 </style>
