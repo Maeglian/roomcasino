ca4f323b4dc0bb9f609f154cfc8c68551b13229b
mariaisaeva <misaeva@wakeapp.ru>
2020-10-30 17:34:22 +0300

    request to /getBillingSession each time when form is opened

--- src/components/CashierForm.vue
+++ src/components/CashierForm.vue
@@ -27,49 +27,48 @@ export default {
     ...mapMutations(['setCashoutFalse']),
     ...mapActions(['getBillingSession', 'getProfile']),
     initializeCashier() {
-      const method = this.shouldCashout ? 'withdrawal' : 'deposit';
-      // eslint-disable-next-line no-unused-vars
-      const CashierInstance = new _PaymentIQCashier('#cashier', {
-        merchantId: this[billingSession].merchantId,
-        userId: this[billingSession].userId,
-        sessionId: this[billingSession].sessionId,
-        environment: 'test',
-        containerHeight: '95vh',
-        method,
-      }, (api) => {
-        api.on({
-          cashierInitLoad: () => console.log('Cashier init load'),
-          update: (data) => console.log('The passed in data was set', data),
-          success: (data) => {
-            console.log('Transaction was completed successfully', data);
-            this.getProfile();
-          },
-          failure: (data) => console.log('Transaction failed', data),
-          isLoading: (data) => console.log('Data is loading', data),
-          doneLoading: (data) => console.log('Data has been successfully downloaded', data),
-          newProviderWindow: (data) => console.log('A new window / iframe has opened', data),
-          paymentMethodSelect: (data) => console.log('Payment method was selected', data),
-          paymentMethodPageEntered: (data) => console.log('New payment method page was opened', data),
-          navigate: (data) => console.log('Path navigation triggered', data),
-        });
-        api.set({
-          config: {
-            amount: 10,
-          },
-        });
-        api.css(`
+      this.getBillingSession().then(() => {
+        const method = this.shouldCashout ? 'withdrawal' : 'deposit';
+        // eslint-disable-next-line no-unused-vars
+        const CashierInstance = new _PaymentIQCashier('#cashier', {
+          merchantId: this[billingSession].merchantId,
+          userId: this[billingSession].userId,
+          sessionId: this[billingSession].sessionId,
+          environment: 'test',
+          containerHeight: '95vh',
+          method,
+        }, (api) => {
+          api.on({
+            cashierInitLoad: () => console.log('Cashier init load'),
+            update: (data) => console.log('The passed in data was set', data),
+            success: (data) => {
+              console.log('Transaction was completed successfully', data);
+              this.getProfile();
+            },
+            failure: (data) => console.log('Transaction failed', data),
+            isLoading: (data) => console.log('Data is loading', data),
+            doneLoading: (data) => console.log('Data has been successfully downloaded', data),
+            newProviderWindow: (data) => console.log('A new window / iframe has opened', data),
+            paymentMethodSelect: (data) => console.log('Payment method was selected', data),
+            paymentMethodPageEntered: (data) => console.log('New payment method page was opened', data),
+            navigate: (data) => console.log('Path navigation triggered', data),
+          });
+          api.set({
+            config: {
+              amount: 10,
+            },
+          });
+          api.css(`
           .your-custom-css {
             color: blue;
           }
         `);
+        });
       });
     },
     onCloseCashierForm() {
       if (this.shouldCashout) this.setCashoutFalse();
     },
-  },
-  created() {
-    this.getBillingSession();
   },
 };
 </script>
