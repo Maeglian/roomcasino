34aa3b7a2f32f7aceaaf3a1845b0622b44306f53
mariaisaeva <misaeva@wakeapp.ru>
2020-11-03 14:08:01 +0300

    corrected ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
       exec_mode: 'cluster',
       instances: 'max',
       watch: false,
-      script: './node_modules/.bin/nuxt start',
+      script: './node_modules/nuxt/bin/nuxt.js',
       args: 'start'
     }
   ]
