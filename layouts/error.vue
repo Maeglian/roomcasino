3509b33208a97f7a8b4dc009a4b472cd8f8d1c55
mariaisaeva <misaeva@wakeapp.ru>
2020-11-06 00:21:31 +0300

    corrected text styles

--- layouts/error.vue
+++ layouts/error.vue
@@ -4,15 +4,15 @@
     <div class="ErrorPage-Content">
       <template v-if="error.statusCode === 404">
         <img class="ErrorPage-Image" src="@/assets/img/not-found.png" alt="Page not found" />
-        <h1 class="ErrorPage-Title">
+        <h1 class="Title Title--type-h1 ErrorPage-Title">
           Page you were looking for was not found
         </h1>
-        <div class="ErrorPage-Text">
+        <div class="Text Text--additional ErrorPage-Text">
           Sorry, we coudn't find the page you were looking for. We suggest that you return to main page.
         </div>
       </template>
       <template v-else>
-        <h1 class="ErrorPage-Title">
+        <h1 class="Title Title--type-h1 ErrorPage-Title">
           An error occurred
         </h1>
       </template>
@@ -35,9 +35,6 @@ export default {
 .ErrorPage {
   &-Title {
     margin-bottom: 12px;
-    font-size: 24px;
-    font-weight: 700;
-    text-transform: uppercase;
     line-height: 1.27;
     text-align: center;
     color: var(--color-text-main);
@@ -45,7 +42,6 @@ export default {
     @media(min-width: $screen-s) {
       width: 559px / $screen-xl * 100%;
       margin-bottom: 17px;
-      font-size: 33px;
     }
   }
 
@@ -76,10 +72,7 @@ export default {
   }
 
   &-Text {
-    font-size: 14px;
-    line-height: 1.66;
     text-align: center;
-    color: var(--color-text-ghost);
 
     @media(min-width: $screen-s) {
       width: 559px / $screen-xl * 100%;
