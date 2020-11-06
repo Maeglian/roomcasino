89667f135996650b11ba91578efcc7bb488e5bbe
mariaisaeva <misaeva@wakeapp.ru>
2020-11-06 13:08:21 +0300

    fixed bug with vuelidate in nuxt

--- nuxt.config.js
+++ nuxt.config.js
@@ -36,7 +36,7 @@ export default {
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
-    '@/plugins/globals.client',
+    '@/plugins/globals.client', '@/plugins/globals'
   ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
--- plugins/globals.client.js
+++ plugins/globals.client.js
@@ -1,11 +1,8 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
 import Vue from 'vue';
 import VModal from 'vue-js-modal';
 import vClickOutside from 'v-click-outside';
-import Vuelidate from 'vuelidate';
 import MarqueeText from 'vue-marquee-text-component'
 
 Vue.use(vClickOutside);
-Vue.use(Vuelidate);
 Vue.use(VModal);
 Vue.component('marquee-text', MarqueeText);
