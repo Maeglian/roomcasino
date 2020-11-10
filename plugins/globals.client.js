8ca86d8cfc6aafde99169cad014f33165926e446
mariaisaeva <misaeva@wakeapp.ru>
2020-11-10 18:14:07 +0300

    added range plugin

--- plugins/globals.client.js
+++ plugins/globals.client.js
@@ -1,8 +1,10 @@
 import Vue from 'vue';
 import VModal from 'vue-js-modal';
 import vClickOutside from 'v-click-outside';
-import MarqueeText from 'vue-marquee-text-component'
+import MarqueeText from 'vue-marquee-text-component';
+import VueSlider from 'vue-slider-component';
 
 Vue.use(vClickOutside);
 Vue.use(VModal);
+Vue.component('VueRange', VueSlider);
 Vue.component('marquee-text', MarqueeText);
