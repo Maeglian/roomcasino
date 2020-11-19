5014c29943dca849b54cc91949218fe55251f9ab
mariaisaeva <misaeva@wakeapp.ru>
2020-11-19 23:29:23 +0300

    corrected bug with maximum-scale

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
     title: 'roomcasino',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1' },
       { hid: 'description', name: 'description', content: '' },
     ],
     link: [
