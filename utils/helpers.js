0f4e6479540301af5a54f7f862a2ee9813dcea96
mariaisaeva <misaeva@wakeapp.ru>
2020-11-27 23:04:24 +0300

    ROOM-4 added function delete from localStorage

--- utils/helpers.js
+++ utils/helpers.js
@@ -25,6 +25,7 @@ export const throttle = (func, ms = 100) => {
 
 export const getObjValuesFromLocalStorage = obj => {
   for (const key in obj) {
+    if (obj[key].children) getObjValuesFromLocalStorage(obj[key].children);
     const storedValue = localStorage.getItem(key);
     if (storedValue) obj[key].value = JSON.parse(storedValue);
   }
@@ -32,6 +33,16 @@ export const getObjValuesFromLocalStorage = obj => {
 
 export const writeObjValuesToLocalStorage = obj => {
   for (const key in obj) {
-    localStorage.setItem(key, JSON.stringify(obj[key].value));
+    if (obj[key].children) writeObjValuesToLocalStorage(obj[key].children);
+    if (obj[key].value) localStorage.setItem(key, JSON.stringify(obj[key].value));
+    if (!obj[key].value && localStorage.getItem(key)) localStorage.removeItem(key);
+  }
+};
+
+export const deleteObjValuesFromLocalStorage = obj => {
+  for (const key in obj) {
+    if (obj[key].children) deleteObjValuesFromLocalStorage(obj[key].children);
+    obj[key].value = '';
+    localStorage.removeItem(key);
   }
 };
