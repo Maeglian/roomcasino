ab419c1c308198b46c70b751bdcc546908e0e18f
mariaisaeva <misaeva@wakeapp.ru>
2020-11-27 22:00:49 +0300

    ROOM-4 corrected BaseInput

--- components/base/BaseInput.vue
+++ components/base/BaseInput.vue
@@ -3,22 +3,30 @@
     <div v-if="v && v.required === false && v.$dirty" class="BaseInput-Error" :class="errorClass">
       Can't be blank
     </div>
-    <div v-if="v && v.email === false" class="BaseInput-Error" :class="errorClass">
+    <div v-if="v && v.email === false && v.$dirty" class="BaseInput-Error" :class="errorClass">
       No valid email address
     </div>
-    <div v-if="v && v.sameAsPassword === false" class="BaseInput-Error" :class="errorClass">
+    <div
+      v-if="v && v.sameAsPassword === false && v.$dirty"
+      class="BaseInput-Error"
+      :class="errorClass"
+    >
       Passwords are not the same
     </div>
-    <div v-if="v && v.minLength === false" class="BaseInput-Error" :class="errorClass">
+    <div v-if="v && v.minLength === false && v.$dirty" class="BaseInput-Error" :class="errorClass">
       Must have at least {{ v.$params.minLength.min }} characters
     </div>
-    <div v-if="v && v.maxLength === false" class="BaseInput-Error" :class="errorClass">
+    <div v-if="v && v.maxLength === false && v.$dirty" class="BaseInput-Error" :class="errorClass">
       Must not be more than {{ v.$params.maxLength.max }} characters
     </div>
-    <div v-if="v && v.passwordCheck === false" class="BaseInput-Error" :class="errorClass">
+    <div
+      v-if="v && v.passwordCheck === false && v.$dirty"
+      class="BaseInput-Error"
+      :class="errorClass"
+    >
       Must be 8+ chars with 1 number, 1 small and 1 capital letter
     </div>
-    <div v-if="v && v.numeric === false" class="BaseInput-Error" :class="errorClass">
+    <div v-if="v && v.numeric === false && v.$dirty" class="BaseInput-Error" :class="errorClass">
       Must be numeric
     </div>
     <slot name="beforeInput-relative"></slot>
@@ -33,6 +41,7 @@
         :autocorrect="autocorrect"
         :autocomplete="autocomplete"
         :disabled="disabled"
+        :inputmode="inputmode"
       />
       <slot name="afterInput-absolute"></slot>
     </div>
@@ -55,19 +64,19 @@ export default {
       default: false,
     },
     inputClass: {
-      type: String,
+      type: [String, Boolean],
       required: false,
-      default: '',
+      default: false,
     },
     wrapperClass: {
-      type: String,
+      type: [String, Boolean],
       required: false,
-      default: '',
+      default: false,
     },
     errorClass: {
-      type: String,
+      type: [String, Boolean],
       required: false,
-      default: '',
+      default: false,
     },
     inputType: {
       type: String,
@@ -90,6 +99,11 @@ export default {
       default: false,
     },
     autocomplete: {
+      type: [String, Boolean],
+      isRequired: false,
+      default: false,
+    },
+    inputmode: {
       type: [String, Boolean],
       isRequired: false,
       default: false,
