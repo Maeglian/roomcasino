ab84a30ed061d72c0c08a57d4b7a0e79ff5e85e5
mariaisaeva <misaeva@wakeapp.ru>
2020-12-01 20:04:33 +0300

    ROOM-78 added constant period variables to config

--- config.js
+++ config.js
@@ -1,3 +1,23 @@
 export const BILLING_PROVIDER_ID = 'devcode';
 export const API_HOST_SANDBOX = 'http://az-44.sandbox.aramuz.net/frontapi/roomcasino';
 export const API_HOST_PROD = 'https://dev.aramuz.net/frontapi/roomcasino';
+
+export const LIMIT_TYPES = [
+  { name: 'Deposit limits', value: 'depositLimit' },
+  { name: 'Session limits', value: 'sessionLimit' },
+];
+
+export const LIMIT_PERIODS = [
+  {
+    name: 'daily',
+    value: 'dayLimit',
+  },
+  {
+    name: 'weekly',
+    value: 'weekLimit',
+  },
+  {
+    name: 'monthly',
+    value: 'monthLimit',
+  },
+];
