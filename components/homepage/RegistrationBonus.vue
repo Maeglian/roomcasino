dcdb7b92819e0398f30a3b247ef4dcd9505a0746
mariaisaeva <misaeva@wakeapp.ru>
2020-12-03 01:15:53 +0300

    ROOM-38 added RegistrationBonus component

---
+++ components/homepage/RegistrationBonus.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="Modal">
+    <div class="Close Modal-Close" @click="$emit('close')"></div>
+    <div class="RegistrationBonus">
+      <div class="RegistrationBonus-Content">
+        <div class="RegistrationBonus-Title">
+          Thank you for registration
+        </div>
+        <div class="RegistrationBonus-Promo">
+          €150
+        </div>
+        <div class="RegistrationBonus-PromoMin">
+          + 55 Free spins
+        </div>
+        <div class="RegistrationBonus-Text">
+          For yours first deposit! Use advantage of RoomСasino Welcome Bonus.
+        </div>
+      </div>
+      <button type="btn" class="Btn Btn--full" @click="onClickBtn">
+        Deposit
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RegistrationBonus',
+  methods: {
+    onClickBtn() {
+      this.$modal.show('cashier');
+      this.$emit('close');
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.RegistrationBonus {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: center;
+  background-color: var(--color-bg);
+  background-image: url('../../assets/img/registration_success.png');
+  background-repeat: no-repeat;
+  background-position: top right;
+  background-size: 100% auto;
+
+  @media (min-width: $screen-xs) {
+    background-size: auto auto;
+  }
+
+  &-Content {
+    max-width: 239px / 328px * 100%;
+    padding-top: 207px / 288px * 100%;
+    text-align: center;
+  }
+
+  &-Title {
+    margin-bottom: 5px;
+    font-size: 20px;
+    font-weight: 700;
+    color: var(--color-text-main);
+    text-transform: uppercase;
+  }
+
+  &-Promo {
+    font-size: 76px;
+    font-weight: 700;
+    line-height: 1;
+    color: var(--color-main1);
+  }
+
+  &-PromoMin {
+    margin-bottom: 5px;
+    font-size: 22px;
+    font-weight: 700;
+    color: var(--color-main1);
+  }
+
+  &-Text {
+    margin-bottom: 28px;
+    font-size: 12px;
+    line-height: 1.6;
+    color: var(--color-text-ghost);
+  }
+}
+</style>
