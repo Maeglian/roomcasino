a7c96fcd1c1abc2efc3860c6daa2011a042b5ff9
mariaisaeva <misaeva@wakeapp.ru>
2020-12-02 20:28:36 +0300

    ROOM-60 added default provider('All providers') to config

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
 export const BILLING_PROVIDER_ID = 'devcode';
 export const API_HOST_SANDBOX = 'http://az-44.sandbox.aramuz.net/frontapi/roomcasino';
 export const API_HOST_PROD = 'https://dev.aramuz.net/frontapi/roomcasino';
+
+export const DEFAULT_PROVIDER = { name: 'All providers', noIcon: true };
