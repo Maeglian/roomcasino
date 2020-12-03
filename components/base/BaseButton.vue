378d65a51bd0bcb3e3e4009d4cf55a57dda76f7e
mariaisaeva <misaeva@wakeapp.ru>
2020-12-03 11:06:00 +0300

    removed classes from BaseButton

--- components/base/BaseButton.vue
+++ components/base/BaseButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <button class="BaseButton">
+  <button class="Btn">
     <Spinner v-if="isLoading" class="Btn-Spinner" />
     <slot></slot>
   </button>
@@ -22,75 +22,3 @@ export default {
   },
 };
 </script>
-
-<style lang="scss">
-.BaseButton {
-  display: inline-flex;
-  justify-content: center;
-  align-items: center;
-  padding-right: 15px;
-  padding-left: 15px;
-  font-size: 16px;
-  font-weight: 700;
-  text-align: center;
-  color: var(--color-text-main);
-  text-transform: uppercase;
-  text-decoration: none;
-  background: var(--color-main1);
-  border: none;
-  cursor: pointer;
-
-  @media (min-width: $screen-m) {
-    font-size: 14px;
-  }
-
-  @media (min-width: $screen-l) {
-    padding-right: 30px;
-    padding-left: 30px;
-  }
-
-  @media (min-width: $screen-xl) {
-    padding-right: 45px;
-    padding-left: 45px;
-    font-size: 16px;
-  }
-
-  &--text {
-    background: none;
-  }
-
-  &--color {
-    background: var(--color-main2);
-
-    &:hover {
-      background: var(--color-hover);
-    }
-  }
-
-  &--dark {
-    background: var(--color-bg-lighter);
-
-    &:hover {
-      background: var(--color-main1);
-    }
-  }
-
-  &--darkColor {
-    background: rgba(235, 28, 42, 0.4);
-  }
-
-  &--outline {
-    background: transparent;
-    border: 2px solid var(--color-main2);
-  }
-
-  &--centered {
-    margin-left: 50%;
-    transform: translateX(-50%);
-  }
-
-  &--full {
-    width: 100%;
-  }
-}
-</style>
