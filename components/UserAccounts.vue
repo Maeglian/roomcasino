d58ad44d03b3c6f7375c47843c1cc8b21de9700a
mariaisaeva <misaeva@wakeapp.ru>
2020-12-22 20:56:46 +0300

    ROOM-185 moved user account section to UserAccounts

---
+++ components/UserAccounts.vue
@@ -0,0 +1,121 @@
+<template>
+  <div class="UserAccounts">
+    <div v-if="isLoggedIn" class="UserAccounts-UserFullname">
+      {{ getFullUserName }}
+    </div>
+    <AttachedPopup v-if="isLoggedIn" v-model="isOpenPopup" @close-popup="closePopup">
+      <div slot="trigger" class="UserAccounts-Balance" @click="togglePopup">
+        <span class="UserAccounts-Value">{{ balance ? balance : activeAccount.balance }}</span>
+        <span class="UserAccounts-Currency">{{
+          currency ? currency : activeAccount.currency
+        }}</span>
+        <i
+          class="UserAccounts-Balance--arrow Nav-Arrow ThinArrow"
+          :class="{ 'Nav-Arrow--Active': isOpenPopup }"
+        />
+      </div>
+
+      <ul class="UserAccounts-Popup">
+        <li
+          v-for="({ balance, currency }, idx) in user.accountList"
+          :key="idx"
+          class="UserAccounts-PopupItem"
+          @click="selectCurrencyAccount({ balance, currency })"
+        >
+          {{ balance }}
+          {{ currency }}
+        </li>
+      </ul>
+    </AttachedPopup>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapState } from 'vuex';
+
+export default {
+  name: 'UserAccounts',
+  data: () => ({
+    balance: null,
+    currency: null,
+    isOpenPopup: false,
+  }),
+  computed: {
+    ...mapState(['user']),
+    ...mapGetters(['activeAccount', 'isLoggedIn']),
+    getFullUserName() {
+      const { firstName, lastName } = this.user;
+      return `${firstName} ${lastName}`;
+    },
+  },
+  methods: {
+    togglePopup() {
+      this.isOpenPopup = !this.isOpenPopup;
+    },
+    closePopup() {
+      this.isOpenPopup = false;
+    },
+    selectCurrencyAccount({ balance, currency }) {
+      this.balance = balance;
+      this.currency = currency;
+      this.closePopup();
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.UserAccounts {
+  &-UserFullname {
+    margin-right: 20px;
+    font-size: 10px;
+    color: var(--color-text-main);
+  }
+
+  &-Popup {
+    overflow: hidden;
+    color: var(--color-text-main);
+  }
+
+  &-PopupItem {
+    margin-bottom: 4px;
+    padding: 16px 13px;
+    font-size: 12px;
+    background: var(--color-bg);
+    cursor: pointer;
+    transition: 0.2s all;
+
+    &:hover {
+      color: var(--color-main1);
+    }
+  }
+
+  &-Balance {
+    display: flex;
+    align-items: center;
+    font-size: 10px;
+    cursor: pointer;
+
+    &--arrow {
+      transform: rotate(45deg) translateY(-40%);
+    }
+  }
+
+  &-Value {
+    color: var(--color-text-main);
+  }
+
+  &-Currency {
+    margin-left: 3px;
+    color: var(--color-text-ghost);
+  }
+}
+
+.Nav-Arrow {
+  transition: 0.2s all;
+
+  &--Active {
+    transform: rotate(225deg);
+  }
+}
+</style>
--- components/homepage/BalanceSection.vue
+++ components/homepage/BalanceSection.vue
@@ -1,97 +1,41 @@
 <template>
-  <div class="GamePanel">
-    <div class="GamePanel-Account">
-      <p v-if="isLoggedIn" class="GamePanel-UserFullname">
-        {{ getFullUserName }}
-      </p>
-
-      <AttachedPopup v-if="isLoggedIn" v-model="isOpenPopup" @close-popup="closePopup">
-        <div slot="trigger" class="GamePanel-Balance" @click="togglePopup">
-          <span class="GamePanel-Balance--Value">{{
-            balance ? balance : activeAccount.balance
-          }}</span>
-          <span class="GamePanel-Balance--Currency">{{
-            currency ? currency : activeAccount.currency
-          }}</span>
-          <i
-            class="GamePanel-Balance--Arrow Nav-Arrow ThinArrow"
-            :class="{ 'Nav-Arrow--Active': isOpenPopup }"
-          />
-        </div>
-
-        <ul class="GamePanel-Popup">
-          <li
-            v-for="({ balance, currency }, idx) in user.accountList"
-            :key="idx"
-            class="GamePanel-PopupItem"
-            @click="selectCurrencyAccount({ balance, currency })"
-          >
-            {{ balance }}
-            {{ currency }}
-          </li>
-        </ul>
-      </AttachedPopup>
-
+  <div class="BalanceSection">
+    <UserAccounts class="BalanceSection-UserAccounts" />
+    <button
+      v-if="isLoggedIn"
+      class="Btn AuthSection-Btn AuthSection-Btn--deposit"
+      @click="$modal.show('cashier')"
+    >
+      Deposit
+    </button>
+    <div v-else class="AuthSection-Login">
       <button
-        v-if="isLoggedIn"
-        class="Btn AuthSection-Btn AuthSection-Btn--deposit"
-        @click="$modal.show('cashier')"
+        class="Btn AuthSection-Btn AuthSection-Btn--login"
+        @click="showRegistrationDialog('login')"
       >
-        Deposit
+        Login
       </button>
-
-      <div v-else class="AuthSection-Login">
-        <button
-          class="Btn AuthSection-Btn AuthSection-Btn--login"
-          @click="showRegistrationDialog('login')"
-        >
-          Login
-        </button>
-        <button class="Btn AuthSection-Btn" @click="showRegistrationDialog('registration')">
-          Register
-        </button>
-      </div>
+      <button class="Btn AuthSection-Btn" @click="showRegistrationDialog('registration')">
+        Register
+      </button>
     </div>
   </div>
 </template>
 
 <script>
-import { mapState, mapGetters } from 'vuex';
 import showAuthDialog from '@/mixins/showAuthDialog';
+import UserAccounts from '@/components/UserAccounts';
 
 export default {
+  components: {
+    UserAccounts,
+  },
   mixins: [showAuthDialog],
-  data: () => ({
-    balance: null,
-    currency: null,
-    isOpenPopup: false,
-  }),
-  computed: {
-    ...mapState(['user']),
-    ...mapGetters(['activeAccount']),
-    getFullUserName() {
-      const { firstName, lastName } = this.user;
-      return `${firstName} ${lastName}`;
-    },
-  },
-  methods: {
-    togglePopup() {
-      this.isOpenPopup = !this.isOpenPopup;
-    },
-    closePopup() {
-      this.isOpenPopup = false;
-    },
-    selectCurrencyAccount({ balance, currency }) {
-      this.balance = balance;
-      this.currency = currency;
-      this.closePopup();
-    },
-  },
 };
 </script>
 
 <style lang="scss">
-.GamePanel {
+.BalanceSection {
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -99,76 +43,18 @@ export default {
   font-weight: bold;
   text-transform: uppercase;
 
-  &-Popup {
-    overflow: hidden;
-    color: #fff;
-    background: #10133a;
-    border-radius: 8px;
-  }
-
-  &-PopupItem {
-    padding: 16px 13px;
-    cursor: pointer;
-    transition: 0.2s all;
-
-    &:hover {
-      background: #242857;
-    }
-
-    &:not(:last-child) {
-      border-bottom: 1px solid #1d2047;
-    }
-  }
-
-  &-Account {
-    display: flex;
-    align-items: center;
-    height: 100%;
-  }
-
-  &-UserFullname {
-    margin: 0 20px 0 0;
-    font-size: 10px;
-    color: #fff;
-
-    @media (max-width: $screen-xs) {
-      font-size: 8px;
-    }
-
-    @media (max-width: $screen-xs) {
-      display: none;
-    }
-  }
-
-  &-Balance {
+  &-UserAccounts {
     display: flex;
     align-items: center;
     margin-right: 36px;
-    font-size: 10px;
-    cursor: pointer;
-
-    &--Value {
-      margin: 0;
-      color: #fff;
-    }
-
-    &--Currency {
-      margin: 0;
-      margin-left: 3px;
-      color: #ccc;
-    }
-
-    &--Arrow {
-      transform: rotate(45deg) translateY(-40%);
-    }
   }
-}
 
-.Nav-Arrow {
-  transition: 0.2s all;
+  .UserAccounts-UserFullname {
+    display: none;
 
-  &--Active {
-    transform: rotate(225deg);
+    @media (min-width: $screen-s) {
+      display: block;
+    }
   }
 }
 </style>
