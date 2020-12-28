36d046197d6362b510c7a71f48a67673abafde66
mariaisaeva <misaeva@wakeapp.ru>
2020-12-28 17:02:50 +0300

    ROOM-124 added not-allowed page

---
+++ pages/not-allowed.vue
@@ -0,0 +1,30 @@
+<template>
+  <section class="ErrorPage NotAllowed">
+    <div class="ErrorPage-Content NotAllowed-Content">
+      <img class="ErrorPage-Image" src="@/assets/img/joker_sad.png" alt="Page not found" />
+      <h1 class="Title Title--type-h1 ErrorPage-Title">
+        Oops
+      </h1>
+      <div class="Text Text--additional ErrorPage-Text">
+        Access denied. You don't have permission to access this page.
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'NotAllowed',
+};
+</script>
+
+<style lang="scss">
+.NotAllowed {
+  &-Content {
+    @media (min-width: $screen-s) {
+      padding-top: 120px;
+      background-position: center 70px;
+    }
+  }
+}
+</style>
