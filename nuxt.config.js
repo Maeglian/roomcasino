db253da75240c1f7805e8ec4eacfbbf74a13908f
mariaisaeva <misaeva@wakeapp.ru>
2020-12-28 23:25:00 +0300

    ROOM-207 added extractCSS: true

--- nuxt.config.js
+++ nuxt.config.js
@@ -66,7 +66,9 @@ export default {
   },
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {},
+  build: {
+    extractCSS: true,
+  },
 
   router: { middleware: ['closeNav'] },
   generate: {
