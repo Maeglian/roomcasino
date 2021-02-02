aff8386403a384d35aa2b20a010635eb51fe11ac
mariaisaeva <misaeva@wakeapp.ru>
2021-02-02 13:03:59 +0300

    ROOM-254 added About-us page

---
+++ pages/about-us.vue
@@ -0,0 +1,94 @@
+<template>
+  <section class="SupportPage">
+    <h1 class="Title Title--type-h1">
+      About us
+    </h1>
+    <div class="SupportPage-Text">
+      <h2 class="Title Title--type-h2">
+        Players like you
+      </h2>
+      <p class="Text">
+        We ourselves have played in various online casinos for many years. We have a huge amount of
+        accumulated gambling experience. We have seen hundreds of games and visited dozens of casino
+        websites. We know what you as players need.
+      </p>
+      <h2 class="Title Title--type-h2">
+        What is Roomcasino?
+      </h2>
+      <h3 class="Title Title--type-h3">
+        It is a team of experienced players
+      </h3>
+      <p class="Text">
+        who have a rich amount of experience not only as players at the gambling table, but also
+        working in casino management. RoomCasino is a next-generation high-tech casino with the
+        latest games that have been developed by the best experts in the industry. It provides a
+        support team that is always ready to help you in any situation. Of course, it also offers
+        generous winnings that are just a couple of clicks away, right here and right now. Don’t
+        miss out. Sign up today!
+      </p>
+      <h2 class="Title Title--type-h2">
+        Our philosophy
+      </h2>
+      <h3 class="Title Title--type-h3">
+        We know what a real player needs
+      </h3>
+      <p class="Text">
+        Currently, there are a large number of online casinos on the online gambling marketplace.
+        Each of them typically declares that it is unique and offers a set of features that its
+        competitors are not able to match. Each casino also claims to give its players a unique
+        gaming experience. We at RoomCasino are not going to waste your time and our time repeating
+        the same words to you that you may have already heard many times. All we want to say is that
+        we here at RoomCasino are players just like you. We know what a real player needs.
+      </p>
+    </div>
+    <hr />
+    <div class="SupportPage-Text">
+      <h3 class="Title Title--type-h3">
+        Bonuses
+      </h3>
+      <p class="Text">
+        Our generous "Welcome Bonus" brings additional thrill to excite even the coldest and most
+        cautious of players. Just make your deposit and get back twice as much in your account. And
+        that's not all! Follow the "Bonuses" section on our website.
+      </p>
+      <h3 class="Title Title--type-h3">
+        Games
+      </h3>
+      <p class="Text">
+        We offer a wide selection of games for our players. We really care about our players, and we
+        would like to offer something to satisfy every taste. The games that we offer were created
+        by the best developers in the industry. This means that they have excellent graphics, and
+        they sound great, too. What is more, of course, is that they let you win. In addition, they
+        all work in accordance with the "provably fair" technology.
+      </p>
+      <h3 class="Title Title--type-h3">
+        Payment systems and security
+      </h3>
+      <p class="Text">
+        The first thing that every player needs is the ability to quickly manage their money. No one
+        likes to wait. We are able to offer our players instant deposits and payouts. Before you
+        even have the chance to blink, the money will reach its destination. Choose a currency, make
+        a deposit, take a deep breath, and you are ready to play.
+      </p>
+      <h3 class="Title Title--type-h3">
+        Support
+      </h3>
+      <p class="Text">
+        All players want to be treated well. As experienced players, we ourselves have taken this
+        principle to heart. Though you may encounter certain problems, our team of professionals
+        will always be there to help you in these situations. Since you can contact our specialists
+        by chat or e-mail around the clock, you can be sure to receive assistance in case problems
+        arise.
+      </p>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'AboutUsPage',
+  layout: 'page',
+};
+</script>
+
+<style scoped></style>
