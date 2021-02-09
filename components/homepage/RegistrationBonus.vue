1c1d7913b8e6057180edbf40eea0e1cecea323ee
mariaisaeva <misaeva@wakeapp.ru>
2021-02-09 16:29:42 +0300

    ROOM-278 corrected text

--- components/homepage/RegistrationBonus.vue
+++ components/homepage/RegistrationBonus.vue
@@ -5,7 +5,7 @@
         Thank you for registration
       </div>
       <div class="RegistrationBonus-Promo">
-        up to €150
+        €150
       </div>
       <!--      <div class="RegistrationBonus-PromoMin">-->
       <!--        + 55 Free spins-->
