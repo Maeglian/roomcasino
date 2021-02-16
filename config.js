4c914ac5218d413ffc762adfbf56ad7c79a0b26a
mariaisaeva <misaeva@wakeapp.ru>
2021-02-16 14:38:34 +0300

    corrected deploy script

--- config.js
+++ config.js
@@ -2,15 +2,9 @@ import moment from 'moment';
 
 export const BILLING_PROVIDER_ID = 'devcode';
 export const API_HOST_SANDBOX = 'http://az-44.sandbox.aramuz.net/frontapi/roomcasino';
-const API_HOST_STAGE_ROOMCASINO = 'https://dev.aramuz.net/frontapi/roomcasino';
-const API_HOST_STAGE_NINECASINO = 'https://dev.aramuz.net/frontapi/ninecasino';
+export const API_HOST_STAGE = 'https://dev.aramuz.net/frontapi/roomcasino';
 const API_HOST_PROD_ROOMCASINO = 'https://aramuz.net/frontapi/roomcasino';
 const API_HOST_PROD_NINECASINO = 'https://aramuz.net/frontapi/ninecasino';
-
-export const API_HOST_STAGE =
-  process.env.NUXT_ENV_SLUG === 'roomcasino'
-    ? API_HOST_STAGE_ROOMCASINO
-    : API_HOST_STAGE_NINECASINO;
 
 export const API_HOST_PROD =
   process.env.NUXT_ENV_SLUG === 'roomcasino' ? API_HOST_PROD_ROOMCASINO : API_HOST_PROD_NINECASINO;
