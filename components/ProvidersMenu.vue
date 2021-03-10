0d03aee2bad2587c7f497697b2102968f133f2cc
mariaisaeva <misaeva@wakeapp.ru>
2021-03-10 20:12:23 +0300

    NINE-344 added providers icons from backend

--- components/ProvidersMenu.vue
+++ components/ProvidersMenu.vue
@@ -31,14 +31,9 @@
       @click="onOpen"
     >
       <img
-        v-if="!providersWithoutIcons.includes(providerActive.name)"
+        v-if="providerActive.iconUrl"
         class="ProvidersMenu-ProviderIcon"
-        :src="
-          require(`@/assets/img/${providerActive.name
-            .toLowerCase()
-            .split(' ')
-            .join('')}.svg`)
-        "
+        :src="providerActive.iconUrl"
         alt=""
       />
       <span class="ProvidersMenu-ActiveProvider">
@@ -57,15 +52,10 @@
         @click="onChooseProvider(gameProducerList[i])"
       >
         <img
-          v-if="!providersWithoutIcons.includes(gameProducerList[i].name)"
+          v-if="gameProducerList[i].iconUrl"
           class="ProvidersMenu-ProviderIcon"
-          :src="
-            require(`@/assets/img/${gameProducerList[i].name
-              .toLowerCase()
-              .split(' ')
-              .join('')}.svg`)
-          "
-          alt=""
+          :src="gameProducerList[i].iconUrl"
+          :alt="gameProducerList[i].name"
         />
         <span class="ProvidersMenu-ProviderName">
           {{ gameProducerList[i].name }}
@@ -92,21 +82,11 @@
           class="ProvidersMenu-AddProvider"
           :class="{
             'ProvidersMenu-Provider--active': providerActive.name === item.name,
-            'ProvidersMenu-Provider--noIcon': providersWithoutIcons.includes(item.name),
+            'ProvidersMenu-Provider--noIcon': !item.iconUrl,
           }"
           @click="onChooseProvider(item)"
         >
-          <img
-            v-if="!providersWithoutIcons.includes(item.name)"
-            class="ProvidersMenu-ProviderIcon"
-            :src="
-              require(`@/assets/img/${item.name
-                .toLowerCase()
-                .split(' ')
-                .join('')}.svg`)
-            "
-            alt=""
-          />
+          <img v-if="item.iconUrl" class="ProvidersMenu-ProviderIcon" :src="item.iconUrl" alt="" />
           {{ item.name }}
         </button>
       </div>
