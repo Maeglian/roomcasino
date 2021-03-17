d1309900911f31b7324f4f1dc02c3e7253633dec
mariaisaeva <misaeva@wakeapp.ru>
2021-03-17 18:33:54 +0300

    added meta content params

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,7 +29,11 @@ export default {
     title: 'ninecasino',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1' },
+      {
+        name: 'viewport',
+        content:
+          'width=device-width, height=device-height, initial-scale=1, minimum-scale=1, maximum-scale=1',
+      },
       { hid: 'description', name: 'description', content: '' },
     ],
     link: [
