fa84389d0338ee9e47dfb7039ead69026368908e
mariaisaeva <misaeva@wakeapp.ru>
2021-03-31 21:03:44 +0300

    NINE-358 added language switcher component

---
+++ components/LangSwitcher.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="LangSwitcher">
+    <button type="button" class="LangSwitcher-ActiveLang" @click="isOpen = !isOpen">
+      <img class="LangSwitcher-Icon" :src="require(`@/assets/img/${activeLocale.icon}`)" />
+    </button>
+    <div
+      ref="langSwitcher"
+      v-click-outside="onClickOutside"
+      class="LangSwitcher-Inner"
+      :class="{ 'LangSwitcher-Inner--hidden': !isOpen }"
+      :style="{ top: `calc(-${height}px - 6px)` }"
+    >
+      <NuxtLink
+        v-for="locale in availableLocales"
+        :key="locale.code"
+        class="LangSwitcher-Lang"
+        :to="switchLocalePath(locale.code)"
+      >
+        <img class="LangSwitcher-Icon" :src="require(`@/assets/img/${locale.icon}`)" />
+      </NuxtLink>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LangSwitcher',
+  data() {
+    return {
+      isOpen: false,
+      height: 0,
+    };
+  },
+  computed: {
+    activeLocale() {
+      return this.$i18n.locales.find(i => i.code === this.$i18n.locale);
+    },
+    availableLocales() {
+      return this.$i18n.locales.filter(i => i.code !== this.$i18n.locale);
+    },
+  },
+  mounted() {
+    this.height = this.$refs.langSwitcher.clientHeight;
+    console.log(this.$refs.langSwitcher.clientHeight);
+  },
+  methods: {
+    onClickOutside(e) {
+      if (!e.target.closest('.LangSwitcher-ActiveLang')) this.isOpen = false;
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.LangSwitcher {
+  position: relative;
+
+  &-ActiveLang {
+    display: inline-flex;
+    flex-shrink: 0;
+    justify-content: center;
+    align-items: center;
+    width: 34px;
+    height: 34px;
+    overflow: hidden;
+    background: var(--color-bg2);
+    border-radius: 50%;
+    cursor: pointer;
+  }
+
+  &-Inner {
+    position: absolute;
+    left: 0;
+    z-index: 100;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 34px;
+    padding: 7px 0;
+    background: var(--color-bg2);
+    border-radius: 60px;
+
+    &--hidden {
+      visibility: hidden;
+    }
+  }
+
+  &-Lang {
+    margin-bottom: 6px;
+
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+
+  &-Icon {
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+  }
+}
+</style>
