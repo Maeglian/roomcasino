c2c47c378fff2fcbb30b2e2dfe107eb1d2dd9276
mariaisaeva <misaeva@wakeapp.ru>
2021-04-05 11:50:42 +0300

    NINE-386 corrected query

--- pages/index.vue
+++ pages/index.vue
@@ -36,7 +36,7 @@ export default {
   layout: 'page',
   middleware: [
     function({ route, redirect }) {
-      if (route.path === '/') redirect('/games/top');
+      if (route.path === '/') redirect({ path: '/games/top', query: route.query });
     },
   ],
   data() {
