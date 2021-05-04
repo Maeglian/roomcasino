e1dbf7b292015488a4e66e5eda381b0d8a0f3196
mariaisaeva <misaeva@wakeapp.ru>
2021-05-04 21:53:15 +0300

    NINE-459 removed dispatches from setAccount action

--- store/index.js
+++ store/index.js
@@ -1241,15 +1241,13 @@ export const actions = {
     }
   },
 
-  async setActiveAccount({ commit, dispatch }, payload) {
+  async setActiveAccount({ commit }, payload) {
     try {
       await axios.post(
         `${API_HOST}/setActiveAccount`,
         payload,
         reqConfig(commit, 'pushNotificationAlert'),
       );
-      dispatch('getProfile');
-      dispatch('getLimits');
     } catch (e) {
       commit('pushErrors', e);
     }
