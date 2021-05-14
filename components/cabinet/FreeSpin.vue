907945b2bc6e11db48cd86d5656479b0195ca94b
mariaisaeva <misaeva@wakeapp.ru>
2021-05-14 20:28:34 +0300

    NINE-305 added FreeSpin component

---
+++ components/cabinet/FreeSpin.vue
@@ -0,0 +1,112 @@
+<template>
+  <div class="Bonus FreeSpin BonusesPage-FreeSpin">
+    <div class="FreeSpin-Section FreeSpin-Section--left">
+      <div class="Bonus-Text">
+        {{ spin.name }}
+      </div>
+      <svg class="Bonus-Icon" width="14" height="14" @click="showBonusDetails(spin, 'freeSpin')">
+        <use xlink:href="@/assets/img/icons.svg#info"></use>
+      </svg>
+    </div>
+    <div class="FreeSpin-Section">
+      <div class="FreeSpin-Amount">
+        <svg class="BonusesPage-Icon" width="16" height="16">
+          <use xlink:href="@/assets/img/icons.svg#promotions"></use>
+        </svg>
+        {{ spin.amount }} Free Spins
+      </div>
+      <button
+        v-if="spin.status !== 'active'"
+        class="Btn Btn--common Btn--color CabinetPage-Btn"
+        @click="onActivateFreeSpin(spin.id)"
+      >
+        Activate now
+      </button>
+      <div v-else class="FreeSpin-Status">
+        {{ spin.status }}
+      </div>
+    </div>
+    <div class="FreeSpin-Section">
+      <button
+        v-if="spin.status === 'notActive'"
+        class="Btn Btn--dark FreeSpin-Btn"
+        @click="onDeleteFreeSpin(spin.id)"
+      >
+        Cancel
+      </button>
+      <div class="FreeSpin-Expires">
+        <div class="FreeSpin-Label">
+          <template v-if="spin.status === 'notActive'">
+            Until activate
+          </template>
+        </div>
+        <Counter
+          class="Bonus-Counter"
+          :min-format="true"
+          :enddate="
+            spin.status === 'notActive'
+              ? new Date(spin.activationExpireAt * 1000)
+              : new Date(spin.expireAt * 1000)
+          "
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapActions, mapGetters, mapMutations, mapState } from 'vuex';
+import BonusDetails from '@/components/cabinet/BonusDetails';
+
+export default {
+  name: 'FreeSpin',
+  props: {
+    spin: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      gameIdToActivate: '',
+    };
+  },
+  computed: {
+    ...mapState(['deleteBonusError', 'activateFreeSpinError']),
+    ...mapGetters(['activeCurrency']),
+  },
+  methods: {
+    ...mapMutations(['pushNotificationAlert']),
+    ...mapActions(['deleteFreeSpin', 'activateFreeSpin', 'getFreeSpinList']),
+    showBonusDetails(bonus, type) {
+      this.$modal.show(
+        BonusDetails,
+        { bonus, type, chooseGame: this.chooseGame },
+        { width: 400, height: 'auto', adaptive: true },
+        {},
+      );
+    },
+    chooseGame(id) {
+      this.gameIdToActivate = id;
+    },
+    onDeleteFreeSpin(id) {
+      this.deleteFreeSpin(id).then(() => {
+        if (this.deleteBonusError)
+          this.pushNotificationAlert({ type: 'error', text: 'Error on cancelling free spin' });
+        else this.pushNotificationAlert({ type: 'success', text: 'Your free spin was cancelled' });
+        this.getFreeSpinList();
+      });
+    },
+    onActivateFreeSpin(id) {
+      this.activateFreeSpin({ id, gameId: this.gameIdToActivate }).then(() => {
+        if (this.activateFreeSpinError)
+          this.pushNotificationAlert({ type: 'error', text: 'Error on activating free spin' });
+        else this.pushNotificationAlert({ type: 'success', text: 'Your free spin was activated' });
+        this.getFreeSpinList();
+      });
+    },
+  },
+};
+</script>
+
+<style scoped></style>
--- pages/cabinet/bonuses.vue
+++ pages/cabinet/bonuses.vue
@@ -53,64 +53,13 @@
       </div>
     </div>
     <div v-if="freeSpinList.length" class="BonusesPage-BonusList">
-      <div v-for="spin in freeSpinList" :key="spin.id" class="Bonus FreeSpin BonusesPage-FreeSpin">
-        <div class="FreeSpin-Section FreeSpin-Section--left">
-          <div class="Bonus-Text">
-            {{ spin.name }}
-          </div>
-          <svg
-            class="Bonus-Icon"
-            width="14"
-            height="14"
-            @click="showBonusDetails(spin, 'freeSpin')"
-          >
-            <use xlink:href="@/assets/img/icons.svg#info"></use>
-          </svg>
-        </div>
-        <div class="FreeSpin-Section">
-          <div class="FreeSpin-Amount">
-            <svg class="BonusesPage-Icon" width="16" height="16">
-              <use xlink:href="@/assets/img/icons.svg#promotions"></use>
-            </svg>
-            {{ spin.amount }} Free Spins
-          </div>
-          <button
-            v-if="spin.status !== 'active'"
-            class="Btn Btn--common Btn--color CabinetPage-Btn"
-            @click="onActivateFreeSpin(spin.id)"
-          >
-            Activate now
-          </button>
-          <div v-else class="FreeSpin-Status">
-            {{ spin.status }}
-          </div>
-        </div>
-        <div class="FreeSpin-Section">
-          <button
-            v-if="spin.status === 'notActive'"
-            class="Btn Btn--dark FreeSpin-Btn"
-            @click="onDeleteFreeSpin(spin.id)"
-          >
-            Cancel
-          </button>
-          <div class="FreeSpin-Expires">
-            <div class="FreeSpin-Label">
-              <template v-if="spin.status === 'notActive'">
-                Until activate
-              </template>
-            </div>
-            <Counter
-              class="Bonus-Counter"
-              :min-format="true"
-              :enddate="
-                spin.status === 'notActive'
-                  ? new Date(spin.activationExpireAt * 1000)
-                  : new Date(spin.expireAt * 1000)
-              "
-            />
-          </div>
-        </div>
-      </div>
+      <FreeSpin
+        v-for="spin in freeSpinList"
+        :key="spin.id"
+        :spin="spin"
+        class="Bonus FreeSpin
+      BonusesPage-FreeSpin"
+      />
     </div>
     <Loader v-if="availableBonusListIsLoading" />
     <div v-if="availableBonusList.length" class="Table CabinetPage-Table BonusesPage-Table">
@@ -229,12 +178,14 @@ import Counter from '@/components/Counter';
 import BonusDetails from '@/components/cabinet/BonusDetails';
 import Loader from '@/components/Loader';
 import CancelBonusPopup from '@/components/CancelBonusPopup';
+import FreeSpin from '@/components/cabinet/FreeSpin';
 
 export default {
   name: 'BonusesPage',
   components: {
     Counter,
     Loader,
+    FreeSpin,
   },
   computed: {
     ...mapState([
@@ -248,8 +199,6 @@ export default {
       'availableFreeSpinList',
       'availableBonusListIsLoading',
       'availableFreeSpinListIsLoading',
-      'deleteBonusError',
-      'activateFreeSpinError',
     ]),
     ...mapGetters(['activeCurrency']),
   },
@@ -270,8 +219,6 @@ export default {
       'getBonusHistoryList',
       'getFreeSpinList',
       'getAvailableFreeSpinList',
-      'deleteFreeSpin',
-      'activateFreeSpin',
     ]),
     onClickCancelBonus(bonus) {
       this.$modal.show(
@@ -299,22 +246,6 @@ export default {
         this.getBonusList();
         this.getBonusHistoryList();
         this.getProfile();
-      });
-    },
-    onDeleteFreeSpin(id) {
-      this.deleteFreeSpin(id).then(() => {
-        if (this.deleteBonusError)
-          this.pushNotificationAlert({ type: 'error', text: 'Error on cancelling free spin' });
-        else this.pushNotificationAlert({ type: 'success', text: 'Your free spin was cancelled' });
-        this.getFreeSpinList();
-      });
-    },
-    onActivateFreeSpin(id) {
-      this.activateFreeSpin(id).then(() => {
-        if (this.activateFreeSpinError)
-          this.pushNotificationAlert({ type: 'error', text: 'Error on activating free spin' });
-        else this.pushNotificationAlert({ type: 'success', text: 'Your free spin was activated' });
-        this.getFreeSpinList();
       });
     },
     showBonusDetails(bonus, type) {
