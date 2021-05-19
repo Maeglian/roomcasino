4b7d4de0abb14c9cdc99731a53c0026fd7203e9b
mariaisaeva <misaeva@wakeapp.ru>
2021-05-19 21:07:35 +0300

    NINE-389 corrected keys in Card

--- components/Card.vue
+++ components/Card.vue
@@ -75,8 +75,8 @@
         </div>
         <div v-if="lastResults" class="Card-Results">
           <div
-            v-for="result in lastResults"
-            :key="result.time"
+            v-for="(result, i) in lastResults"
+            :key="i"
             class="Card-Result"
             :style="{ backgroundColor: result.color }"
           >
