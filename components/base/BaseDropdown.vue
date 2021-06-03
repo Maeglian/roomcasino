c33e63045d453c0bfc42a7555865abdd54aa5cfb
mariaisaeva <misaeva@wakeapp.ru>
2021-06-03 18:25:53 +0300

    NINE-492 corrected base dropdown styles

--- components/base/BaseDropdown.vue
+++ components/base/BaseDropdown.vue
@@ -226,7 +226,7 @@ export default {
     font-weight: 700;
     color: var(--color-text-main);
     text-transform: inherit;
-    background: var(--color-bg);
+    background: transparent;
   }
 
   &-Item {
@@ -239,7 +239,6 @@ export default {
     justify-content: space-between;
     align-items: center;
     width: 100%;
-    background: var(--color-bg);
   }
 
   &-Input {
@@ -271,6 +270,7 @@ export default {
     width: 100%;
     max-height: 200px;
     overflow: auto;
+    background: var(--color-bg);
     scrollbar-width: thin;
     scrollbar-color: #6a6e7f transparent;
 
