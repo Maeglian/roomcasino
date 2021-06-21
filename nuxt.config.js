f9530ae6c915d06e5eafcecc926f1b19bfe169a9
mariaisaeva <misaeva@wakeapp.ru>
2021-06-21 16:59:18 +0300

    NINE-526 corrected sentry config

--- nuxt.config.js
+++ nuxt.config.js
@@ -153,7 +153,9 @@ export default {
       GlobalHandlers: {},
       LinkedErrors: {},
       UserAgent: {},
-      CaptureConsole: {},
+      CaptureConsole: {
+        levels: ['warn', 'error'],
+      },
     },
     config: {
       release: process.env.GITHUB_SHA,
