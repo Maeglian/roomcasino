d8e1f37705983db01d563d36351acb845583942e
mariaisaeva <misaeva@wakeapp.ru>
2021-09-15 14:33:01 +0300

    Revert "NINE-799 added prod config"
    This reverts commit 80e0aaa3

--- config.js
+++ config.js
@@ -18,7 +18,7 @@ export const PRAGMATIC_CASINOID =
 
 export const BILLING_PROVIDER_ID = 'devcode';
 export const API_HOST_SANDBOX = 'http://az-44.sandbox.aramuz.net/frontapi/roomcasino';
-export const API_HOST_STAGE = 'https://aramuz.net/frontapi/ninecasino';
+export const API_HOST_STAGE = 'https://dev.aramuz.net/frontapi/ninecasino';
 export const API_HOST_PROD = 'https://aramuz.net/frontapi/ninecasino';
 
 export const API_HOST =
